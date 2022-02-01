58eacff891aae012cbeedb838a5f2c4b2bc412b4
constantijn <constantijn@vanhartesveldt.nl>
2022-02-01 21:26:20 +0100

    started on header

--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -1,7 +1,12 @@
+import Header from "../components/header"
+
 export default function Home() {
   return (
+    <>
+    <Header/>
       <div className="container mx-auto bg-orange-500">
           <h1>Test</h1>
       </div>
+      </>
   )
 }
