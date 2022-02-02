20053c81093af935876cdd14e1b3b5ab7f875277
constantijn <constantijn@vanhartesveldt.nl>
2022-02-02 12:28:19 +0100

    work

--- my-project/pages/detail/[product].js
+++ my-project/pages/detail/[product].js
@@ -1,25 +1,42 @@
-import Image from 'next/image'
-import { useRouter } from 'next/router'
-import Header from '../../components/header'
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Header from "../../components/header";
 
-function Detail({data}){
-    return(
-        <>
-        <Header/>
-        {console.log(data)}
-        <h1>details</h1>
-
-        {/* TODO ples use next/image */}
-        <img src={data.images[0].src}/>
-        <p>{data.name}</p>
-        </>
-    )
+function Detail({ data }) {
+  const router = useRouter();
+  return (
+    <>
+      <Header />
+      <h1>details</h1>
+      <button
+        onClick={() => router.back()}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        go back
+      </button>
+      {data.images.map((image) => {
+        return (
+          <Image
+            width="200px"
+            height="200px"
+            alt="alt"
+            key={image.alt}
+            src={image.src}
+          />
+        );
+      })}
+      <p>{data.name}</p>
+    </>
+  );
 }
 
-Detail.getInitialProps = async ({query}) => {
-    const res = await fetch(`http://webshop.imaretarded.dev/api/products/view/${query.product}`)
-    const data = await res.json()
-    return { data }
-  }
+Detail.getInitialProps = async ({ query }) => {
+  const res = await fetch(
+    `http://webshop.imaretarded.dev/api/products/view/${query.product}`
+  );
+  const data = await res.json();
+  console.log(data)
+  return { data };
+};
 
-  export default Detail
+export default Detail;
--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -1,28 +1,26 @@
-import Header from "../components/header"
-// import WooCommerce from "../lib/woocommerce"
+import Header from "../components/header";
 
-export default function Home({data}) {
-  // const data = WooCommerce.get("products")
+export default function Home({ data }) {
   return (
     <>
-    <Header/>
+      <Header />
       <div className="container mx-auto bg-orange-500">
-          <h1>Test</h1>
-          {data.map((product)=>{
-            return(
-              <div key={product.id}>
+        <h1>Test</h1>
+        {data.map((product) => {
+          return (
+            <>
                 <h1>{product.name}</h1>
                 <p>{product.id}</p>
-              </div>
-            )
-          })}
+            </>
+          );
+        })}
       </div>
-      </>
-  )
+    </>
+  );
 }
 
 Home.getInitialProps = async (ctx) => {
-  const res = await fetch('http://webshop.imaretarded.dev/api/products/get')
-  const data = await res.json()
-  return { data }
-}
+  const res = await fetch("http://webshop.imaretarded.dev/api/products/get");
+  const data = await res.json();
+  return { data };
+};
