c6cb2d73add9cea2e296ae3521a8ca29028b7d34
constantijn <constantijn@vanhartesveldt.nl>
2022-02-03 10:11:15 +0100

    updated the header

--- my-project/components/header/header.jsx
+++ my-project/components/header/header.jsx
@@ -1,15 +1,28 @@
 export default function Header() {
   return (
-    <div>
-      <nav className="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
-        <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-          <div className="container-fluid">
-            <a className="text-xl text-black" href="/">
-              Logo
-            </a>
-          </div>
-        </div>
-      </nav>
+    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <span class="font-semibold text-xl tracking-tight">logo</span>
     </div>
+    <div class="block lg:hidden">
+      <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow"></div>
+      <div>
+      <a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          home
+        </a>
+        <a href="/catagory" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          catagory
+        </a>
+        <a href="/contact" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          contact
+        </a>
+      </div>
+    </div>
+  </nav>
   );
 }
