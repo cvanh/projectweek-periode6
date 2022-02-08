d0b9769a40966888787a37a46bf473944873464d
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2022-02-08 23:17:51 +0100

    added next/link to home

--- my-project/lib/index.js
+++ my-project/lib/index.js
@@ -2,10 +2,12 @@ const {WooCommerce} = require("./woocommerce")
 const {SetChache} = require("./SetChache")
 const {AddtoCart} = require("./cart/addtocart")
 const {GetCart} = require("./cart/GetCart.js")
+const BetterFetch = require("./BetterFetch")
 
 module.exports = {
     WooCommerce,
     SetChache,
     GetCart,
     AddtoCart,
+    BetterFetch,
 }
--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -14,7 +14,8 @@ export default function Home({data }) {
                     console.log(product);
                     console.log((product.images[0].src))
                     return (
-                        <a key={product.id} href={product.href} className="group">
+                        <Link key={product.id} href={`/detail/${product.id}`} >
+                          <div className="group">
                             <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                                 <img
                                     src={product.images[0].src}
@@ -24,7 +25,8 @@ export default function Home({data }) {
                             </div>
                             <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
                             <p className="mt-1 text-lg font-medium text-gray-900">€{product.price}</p>
-                        </a>
+                            </div>
+                        </Link>
                     );
                 })}
             </div>
