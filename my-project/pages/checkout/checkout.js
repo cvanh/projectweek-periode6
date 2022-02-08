c73e858d436f6ba10b30cfea8f3dde4793a78775
Gavin1103 <gtjin1103@gmail.com>
2022-02-08 11:06:48 +0100

    mijn deel

---
+++ my-project/pages/checkout/checkout.js
@@ -0,0 +1,36 @@
+import Header from "../../components/header";
+function Detail({ data }) {
+    // const router = useRouter();
+    return (
+        <>
+            <Header />
+            <div id="container_checkout">
+
+                <div className="inner_container_links">
+                    <h2 className="titel_overzicht">Winkelwagen</h2>
+                </div>
+                <form id="form_checkout">
+                    <input type="text" placeholder="naam" name="name" id="" />
+                    <input type="text" placeholder="achternaam" name="surname" id="" />
+                    <input type="text" placeholder="E-mail" name="email" id="" />
+                    <input type="text" placeholder="postcode" name="zip-code" id="" />
+                    <input type="text" placeholder="stad" name="city" id="" />
+                    <input type="text" placeholder="straat" name="street" id="" />
+                    <input type="number" placeholder="huisnummer" name="number" id="" />
+                </form>
+            </div>
+        </>
+    );
+}
+
+//   Detail.getInitialProps = async ({ query }) => {
+//     const res = await fetch(
+//       (process.env.NODE_ENV === "production" ?
+//         "http://webshop.imaretarded.dev" : "http://localhost:3000") + `/api/products/view/${query.product}`
+//     )
+//     const data = await res.json();
+//     console.log(data)
+//     return { data };
+//   };
+
+export default Detail;
