891391f67955ad711f3c182fd86e9343ec104613
Justin Berghahn <justin.berghahn@live.nl>
2022-02-10 09:39:31 +0100

    Cards

--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -6,22 +6,26 @@ export default function Home({ Cookies,data }) {
     <>
     {console.log(Cookies)}
       <Header />
+
+
     <div className="bg-white">
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
             <h2 className="sr-only">Products</h2>
 
             <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 {data.map((product) => {
-                    console.log(product);
-                    console.log((product.images[0].src))
+                    console.log(product.images.src);
                     return (
                         <a key={product.id} href={product.href} className="group">
                             <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                                <img
-                                    src={product.images[0].src}
-                                    alt={product.imageAlt}
-                                    className="w-full h-full object-center object-cover group-hover:opacity-75"
-                                />
+                                {product.src}
+                                {product.images.map((image)=>(
+                                    <img
+                                        src={image.src}
+                                        alt={image.alt}
+                                        className="w-full h-full object-center object-cover group-hover:opacity-75"
+                                    />
+                                ))}
                             </div>
                             <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
                             <p className="mt-1 text-lg font-medium text-gray-900">€{product.price}</p>
@@ -31,6 +35,7 @@ export default function Home({ Cookies,data }) {
             </div>
         </div>
     </div>
+
     </>
   );
 }
