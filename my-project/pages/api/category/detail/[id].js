c41f3dab84061bc9e39d7b33221298e98e7c4de9
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2022-02-10 16:06:57 +0100

    fixed api reponse

--- my-project/pages/api/category/detail/[id].js
+++ my-project/pages/api/category/detail/[id].js
@@ -2,13 +2,16 @@ import { SetChache } from "../../../../lib";
 import WooCommerce from "../../../../lib/woocommerce"
 
 export default async function handler(req, res) {    
-    console.log("Aaa")
+    // console.log("Aaa")
     const parameter = {
         min_price: 10
     }
-    const api = await WooCommerce.get(`
-    products?category=${req.query.id}`
-    );
+    const api = await WooCommerce.get(`products`,);
+    // const api = await WooCommerce.get("products");
     res.status(200).json(api.data)
 }
+  
+// export default function handler(req, res) {
+//     res.status(200).json({ name: 'oi piss off' })
+//   }
   
