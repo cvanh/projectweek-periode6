3ce8230e4922caa447795454d3267782d285f0f8
Justin Berghahn <justin.berghahn@live.nl>
2022-02-10 16:32:01 +0100

    Added Footer
    <Footer />

---
+++ my-project/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return(
+        <footer class="bg-gray-50 text-center lg:text-left">
+        <div class="text-gray-700 text-center p-4">
+          © 2021 Copyright:
+          <a class="text-gray-800" href="https://tailwind-elements.com/"> Muziek Winkel</a>
+        </div>
+      </footer>
+    )
+
+}
