3ed14d90627e88e413d067be92098c5a2c7e82ff
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2022-02-10 16:19:19 +0100

    fixed get my catagory

--- my-project/pages/api/category/detail/[catagoryid].js
+++ my-project/pages/api/category/detail/[catagoryid].js
@@ -4,7 +4,7 @@ import WooCommerce from "../../../../lib/woocommerce"
 export default async function handler(req, res) {    
     // console.log("Aaa")
     const parameter = {
-        // category: req.param.category
+        category: req.query.catagoryid
     }
     const api = await WooCommerce.get(`products`,parameter);
     res.status(200).json(api.data)
