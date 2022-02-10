ab5a1c0cbe59b4d2438e40326bf680e891fc3da8
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2022-02-10 16:07:56 +0100

    based filter on catagory

--- my-project/pages/api/category/detail/[id].js
+++ my-project/pages/api/category/detail/[id].js
@@ -4,9 +4,10 @@ import WooCommerce from "../../../../lib/woocommerce"
 export default async function handler(req, res) {    
     // console.log("Aaa")
     const parameter = {
-        min_price: 10
+        min_price: 10,
+        category: 24
     }
-    const api = await WooCommerce.get(`products`,);
+    const api = await WooCommerce.get(`products`,parameter);
     // const api = await WooCommerce.get("products");
     res.status(200).json(api.data)
 }
