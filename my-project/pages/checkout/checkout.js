485cfd13e50ea0c9f042ab61eefe484a014ea7bc
constantijn <constantijn@vanhartesveldt.nl>
2022-02-11 08:38:39 +0100

    fixed checkout

--- my-project/pages/checkout/checkout.js
+++ my-project/pages/checkout/checkout.js
@@ -69,18 +69,18 @@ function Checkout() {
                                 // content && content.map((products)=>(<input type="hidden" value={product.id}/>))
                             }
                             <input type="text" placeholder="naam" name="name" id="" />
-                            <input type="text" placeholder="achternaam" name="surname" id="" />
+                            <input type="text" placeholder="achternaam" name="last_name" id="" />
                             <br></br>
                             <input type="text" placeholder="E-mail" name="email" id="" />
-                            <input type="text" placeholder="postcode" name="zip_code" id="" />
+                            <input type="text" placeholder="postcode" name="postcode" id="" />
                             <br></br>
                             <input type="text" placeholder="stad" name="city" id="" />
                             <input type="text" placeholder="state" name="state" id="" />
                             <br></br>
-                            <input type="text" placeholder="straat" name="street" id="" />
+                            <input type="text" placeholder="straat" name="address_1" id="" />
                             <input type="number" placeholder="huisnummer" name="number" id="" />
                             <br></br>
-                            <input type="phone" placeholder="phone number" name="phonenumber" id="" />
+                            <input type="phone" placeholder="phone number" name="phone" id="" />
                             <input id="bestel_knop" type="submit" value="Bestellen" />
                         </div>
                     </div>
@@ -89,15 +89,5 @@ function Checkout() {
         </>
     );
 }
-
-//   Detail.getInitialProps = async ({ query }) => {
-//     const res = await fetch(
-//       (process.env.NODE_ENV === "production" ?
-//         "http://webshop.imaretarded.dev" : "http://localhost:3000") + `/api/products/view/${query.product}`
-//     )
-//     const data = await res.json();
-//     console.log(data)
-//     return { data };
-//   };
 
 export default Checkout;
